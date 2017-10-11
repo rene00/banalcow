464c2a0f245a23b62cd778989f01ed19263e70b4
Rene Cunningham <rene@compounddata.com>
2017-10-11 21:24:37 +1100

    Added passwordstore support to config.

--- banalcow/config.py
+++ banalcow/config.py
@@ -1,5 +1,6 @@
 import yaml
 from collections import namedtuple
+import pypass
 
 
 class Config:
@@ -7,12 +8,27 @@ class Config:
         self.config_file = config_file
         with open(self.config_file, 'r') as stream:
             self.data = yaml.load(stream)
+        try:
+            self.pypass = pypass.PasswordStore(
+                path=self.data['pypass']['path']
+            )
+        except TypeError:
+            self.pypass = False
 
     @property
     def netbank(self):
         netbank = namedtuple('netbank', 'username password')
-        username = self.data['netbank']['username']
-        password = self.data['netbank']['password']
+        if self.pypass:
+            # FIXME: aviau/python-pass#12
+            username = self.pypass.get_decypted_password(
+                'Personal/netbank', pypass.EntryType.username
+            )
+            password = self.pypass.get_decypted_password(
+                'Personal/netbank', pypass.EntryType.password
+            )
+        else:
+            username = self.data['netbank']['username']
+            password = self.data['netbank']['password']
         return netbank(
             username=str(username),
             password=str(password)
@@ -21,9 +37,21 @@ class Config:
     @property
     def penny(self):
         penny = namedtuple('penny', 'login_url username password')
-        login_url = self.data['penny']['login_url']
-        username = self.data['penny']['username']
-        password = self.data['penny']['password']
+        if self.pypass:
+            # FIXME: aviau/python-pass#12
+            login_url = yaml.load(
+                self.pypass.get_decypted_password('Personal/penny')
+            )['login_url']
+            username = self.pypass.get_decypted_password(
+                'Personal/penny', pypass.EntryType.username
+            )
+            password = self.pypass.get_decypted_password(
+                'Personal/penny', pypass.EntryType.password
+            )
+        else:
+            login_url = self.data['penny']['login_url']
+            username = self.data['penny']['username']
+            password = self.data['penny']['password']
         return penny(
             login_url=str(login_url),
             username=str(username),
