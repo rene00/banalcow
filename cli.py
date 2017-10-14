259d98cf9082421d460afc47e3cefb2d030b8f80
Rene Cunningham <rene@compounddata.com>
2017-10-15 08:07:02 +1100

    Remove IPython and iterate over accounts saving ofx.

--- cli.py
+++ cli.py
@@ -1,9 +1,14 @@
 from banalcow import netbank, config
-from IPython import embed
 
 conf = config.Config('./config.yml')
 session = netbank.Netbank(conf.netbank.username, conf.netbank.password)
 session.login()
-embed()
+
+for account, data in session.accounts.items():
+    session.access_account(account)
+    session.set_date_widget()
+    session.download_ofx(data.filename)
+    session.access_homepage()
+
 session.logout()
 session.driver.quit()
