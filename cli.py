cf6930942fefdc3b34be43a19fcc6f3ae33e401d
Rene Cunningham <rene@compounddata.com>
2017-10-15 16:59:44 +1100

    Completed initial penny support.

--- cli.py
+++ cli.py
@@ -1,14 +1,27 @@
-from banalcow import netbank, config
+from banalcow import netbank, config, penny
 
 conf = config.Config('./config.yml')
 session = netbank.Netbank(conf.netbank.username, conf.netbank.password)
 session.login()
 
-for account, data in session.accounts.items():
+accounts = session.accounts
+
+for account, data in accounts.items():
     session.access_account(account)
     session.set_date_widget()
     session.download_ofx(data.filename)
     session.access_homepage()
 
 session.logout()
+
+if conf.penny:
+    session = penny.Penny(
+        conf.penny.base_url, conf.penny.username, conf.penny.password
+    )
+    session.login()
+    for account, data in accounts.items():
+        session.upload(data.filename)
+
+    session.logout()
+
 session.driver.quit()
