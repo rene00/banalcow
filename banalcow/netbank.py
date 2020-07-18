9cead7803bb612e3893975e82964e7d710dc4a39
Rene Cunningham <rene@compounddata.com>
2020-07-18 23:04:18 +1000

    Update paths based on latest update to website

--- banalcow/netbank.py
+++ banalcow/netbank.py
@@ -10,7 +10,7 @@ from selenium.common.exceptions import (
 import datetime
 import time
 import shutil
-from IPython import embed
+import os
 from banalcow import banalutil
 from banalcow.driver import BanalDriver
 
@@ -27,15 +27,20 @@ class Netbank:
     login_url = "https://www.my.commbank.com.au/netbank/Logon/Logon.aspx"
     date_fmt = "%d/%m/%Y"
 
-    def __init__(self, username, password, **kwargs):
+    def __init__(self, username, password, sleep, retry, **kwargs):
         self.username = username
         self.password = password
+        self.sleep = sleep
+        self.retry = retry
         self.from_date = kwargs.get('from_date')
         self.to_date = kwargs.get('to_date')
         self.proxy = kwargs.get('proxy')
         self.chrome_driver_executable_path = kwargs.get(
             'chrome_driver_executable_path'
         )
+        self.only_home_loans = kwargs.get('only_home_loans', False)
+        self.debug = kwargs.get('debug', False)
+
 
         if self.__from_date > self.__to_date:
             raise NetbankError(
@@ -103,92 +108,96 @@ class Netbank:
         submit_button.click()
         self.homepage = self.driver.current_url
 
-    @property # noqa[C901]
-    def accounts(self):
+    def get_accounts(self):
         """Account information in the form of a dict of tuples."""
 
         # Use WebDriverWait to wait for the presence of the portfolio table.
         # This was taking a bit too long to render sometimes and selenium would
         # throw an exception not being able to find the element in time.
-        table = WebDriverWait(self.driver, 30).until(
+        WebDriverWait(self.driver, self.sleep).until(
             EC.presence_of_element_located(
-                (By.XPATH, '//*[@id="MyPortfolioGrid1_a"]')
+                (By.XPATH, '//*[@id="StartMainContent"]/div/div[2]/div[1]/main/section[1]/div/div[1]')
             )
         )
 
-        accounts = {}
         account = namedtuple(
             'account',
-            'nickname balance funds href filename home_loan'
+            'name balance available href filename home_loan'
         )
 
-        for row in table.find_elements_by_xpath(".//tr"):
-            bsb = None
-            home_loan = False
-            for td in row.find_elements(By.TAG_NAME, 'td'):
-                _class = td.get_attribute('class')
-                if _class == 'BSBField':
-                    bsb = td.text.replace(' ', '')
-                elif _class == 'AccountNumberField':
-                    accountnumber = td.text.replace(' ', '')
-                elif _class == 'NicknameField FirstCol':
-                    nickname = td.text
-                    div = td.find_elements_by_xpath(
-                        '//*[@id="{0}"]/div/div[1]'.
-                        format(td.get_attribute('id'))
-                    )[0]
-                    anchor = div.find_elements_by_xpath('.//a')[0]
-                    href = anchor.get_attribute('href')
-                elif _class == ('AccountBalanceField CurrencyField '
-                                'CurrencyFieldBold'):
-                    balance = td.text
-                elif _class == 'AvailableFundsField CurrencyField LastCol':
-                    funds = td.text
+        accounts = {}
+        count = 1
+        while True:
 
-            # If bsb can be coerced into an int, we are dealing with an
-            # account row.
-            # accountnumber = None
             try:
-                int(bsb)
-            except ValueError:
-                # CBA credit card has 'Awards' as text within the BSB field.
-                if bsb.lower() == 'awards':
-                    pass
-            except TypeError:
-                # BSB is None. Move on.
+                self.driver.find_element_by_xpath('//*[@id="StartMainContent"]/div/div[2]/div[1]/main/section[1]/div/div[1]/div[{0}]'.format(count))
+            except NoSuchElementException:
+                break
+
+            name = self.driver.find_element_by_xpath('//*[@id="StartMainContent"]/div/div[2]/div[1]/main/section[1]/div/div[1]/div[{0}]/div/div[1]/div[2]/div/div/div/a/h3'.format(count))
+
+            if name.get_attribute('title').lower() == 'commsec shares':
+                count += 1
                 continue
-            else:
-                accountnumber = "{0}{1}".format(bsb, accountnumber)
 
-            if accountnumber:
-                # Skip CommSec accounts.
-                if nickname.lower() == 'commsec shares':
-                    continue
+            home_loan = False
+            if name.get_attribute('title').lower() == 'home loan':
+                home_loan = True
 
-                if nickname.lower() == 'home loan':
-                    home_loan = True
+            if self.only_home_loans and not home_loan:
+                count += 1
+                continue
 
-                filename = banalutil.filename(
-                    accountnumber, self.from_date, self.to_date
+            number = self.driver.find_element_by_xpath('//*[@id="StartMainContent"]/div/div[2]/div[1]/main/section[1]/div/div[1]/div[{0}]/div/div[1]/div[2]/div/div/span/div'.format(count))
+            balance = self.driver.find_element_by_xpath('//*[@id="StartMainContent"]/div/div[2]/div[1]/main/section[1]/div/div[1]/div[{0}]/div/div[1]/div[2]/div/ul/li[1]/span[2]'.format(count))
+            available = self.driver.find_element_by_xpath('//*[@id="StartMainContent"]/div/div[2]/div[1]/main/section[1]/div/div[1]/div[{0}]/div/div[1]/div[2]/div/ul/li[2]/span[2]'.format(count))
+            href = self.driver.find_element_by_xpath('//*[@id="StartMainContent"]/div/div[2]/div[1]/main/section[1]/div/div[1]/div[{0}]/div/div[1]/div[2]/div/div/div/a'.format(count))
+
+            # Remove all non-digits from account number
+            accountnumber = ''.join(filter(str.isdigit, number.text))
+
+            if accounts.get(accountnumber):
+                count += 1
+                continue
+
+            filename = banalutil.filename(
+                accountnumber, self.from_date, self.to_date
+            )
+
+            if self.debug:
+                print(
+                    "DEBUG1: {0},{1},{2},{3},{4},{5},{6}".format(
+                        name.get_attribute('title'),
+                        accountnumber,
+                        balance.get_attribute('title'),
+                        available.get_attribute('title'),
+                        href.get_attribute('href'),
+                        filename,
+                        home_loan
+                    )
                 )
-                accounts[accountnumber] = account(
-                    nickname=nickname, balance=balance, funds=funds,
-                    href=href, filename=filename, home_loan=home_loan
-                )
+
+            accounts[accountnumber] = account(
+                name=name.get_attribute('title'),
+                balance=balance.get_attribute('title'),
+                available=available.get_attribute('title'),
+                href=href.get_attribute('href'),
+                filename=filename,
+                home_loan=home_loan
+            )
+
+            count += 1
 
         return accounts
 
     def logout(self):
-        submit_button = self.driver.find_element_by_id(
-            'ctl00_HeaderControl_logOffLink'
-        )
+        submit_button = self.driver.find_element_by_xpath('//*[@id="header"]/div[2]/nav/div[1]/ul/li[3]/a')
         submit_button.click()
 
     def access_homepage(self):
         self.driver.get(self.homepage)
 
-    def access_account(self, accountnumber):
-        href = self.accounts[accountnumber].href
+    def access_account(self, accountnumber, href):
         self.driver.get(href)
 
     def view_transactions(self):
@@ -198,30 +207,35 @@ class Netbank:
         Non Home Loan accounts dont have this link.
         """
         attempts = 0
-        while (attempts < 2):
+        while (attempts < self.retry):
             attempts += 1
             try:
-                search_elem = WebDriverWait(self.driver, 10).until(
+                search_elem = WebDriverWait(self.driver, self.sleep).until(
                     EC.presence_of_element_located(
                         (By.XPATH, '//*[@id="lnk-transactions-viewAll"]')
+
                     )
                 )
-            except (TimeoutException, StaleElementReferenceException):
+            except TimeoutException:
+                break
+
+            try:
+                search_elem.click()
+            except StaleElementReferenceException:
                 pass
             else:
-                search_elem.click()
                 break
 
     def download_ofx(self, filename):
         attempts = 0
-        while (attempts < 2):
+        while (attempts < self.retry):
             attempts += 1
             try:
-                export_elem = WebDriverWait(self.driver, 10).until(
+                export_elem = WebDriverWait(self.driver, self.sleep).until(
                     EC.presence_of_element_located(
                         (
                             By.XPATH,
-                            '//*[@id="ctl00_ToobarFooterRight_updatePanelExport"]/div/a' # noqa[E501]
+                            '//*[@id="ctl00_CustomFooterContentPlaceHolder_updatePanelExport1"]/div'
                         )
                     )
                 )
@@ -231,33 +245,28 @@ class Netbank:
                 TimeoutException
             ) as e:
                 print(e)
-                embed()
             else:
-                export_elem.send_keys(Keys.NULL)
                 export_elem.click()
                 break
-
-        time.sleep(2)
 
         try:
             export_type_elem = Select(
                 self.driver.find_element_by_xpath(
-                    '//*[@id="ctl00_ToobarFooterRight_ddlExportType_field"]'
+                    '//*[@id="ctl00_CustomFooterContentPlaceHolder_ddlExportType1_field"]'
+
                 )
             )
         except (NoSuchElementException, WebDriverException) as e:
             print(e)
-            embed()
         else:
             export_type_elem.select_by_value('OFX')
 
         try:
             submit_button = self.driver.find_element_by_xpath(
-                '//*[@id="ctl00_ToobarFooterRight_lbExport"]'
+                '//*[@id="ctl00_CustomFooterContentPlaceHolder_lbExport1"]'
             )
         except (NoSuchElementException, WebDriverException) as e:
             print(e)
-            embed()
         else:
             submit_button.click()
             time.sleep(2)
